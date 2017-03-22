807785047dfe6fb6021b793fac28303d56609d0d
Claudio Nunez <claudioarchi@hotmail.com>
2017-03-21 23:25:53 -0400

    Adventure Game
    Description of the adventure game. Excercise from the book page 196

---
+++ ex43.py
@@ -0,0 +1,10 @@
+# This code will create an adventure game
+"""
+Description of the game:
+Aliens have invaded a space ship and our hero has to go through a maze of rooms
+defeating them so he can escape into an escape pod to the planet below.
+The gamewill be more like a Zork or Adventure type game with text outputs
+and funny ways to die. The game will involve an engine that runs a map full
+of rooms orscenes. Each room will print its own description when the player
+enters it and then tell the engine what room to run next out of the map.
+"""
